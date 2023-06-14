f8bb2f7479f508e15d2fd0e21fe92c61fba05c93
Peng Ye <yemouren@protonmail.com>
2023-06-14 17:25:27 +0800

    add swapfile

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -7,11 +7,11 @@
         mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
       });
     })
-   # (import (builtins.fetchTarball {
-   #   url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-   #   # sha256 = lib.fakeSha256;
-   #   # sha256 = "1k04sc2h41zx7cp56mxq1g7lzz4nlnwax5z666nqbx7c84gxlm89";
-   # }))
+   (import (builtins.fetchTarball {
+     url = https://ghproxy.com/github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+     # sha256 = lib.fakeSha256;
+     # sha256 = "1k04sc2h41zx7cp56mxq1g7lzz4nlnwax5z666nqbx7c84gxlm89";
+   }))
   ];
 
 }
--- modules/prog.nix
+++ modules/prog.nix
@@ -7,75 +7,6 @@
     home.stateVersion = "23.05";
     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
     nixpkgs.config.allowUnfree = true;
-
-    home.packages = with pkgs; [
-      firefox-wayland
-      wpsoffice
-      # xcape xorg.xmodmap xorg.xwininfo xclip xdotool
-      wmctrl
-      xdg-utils
-      dracula-theme
-      wayland wdisplays mako gnome3.adwaita-icon-theme
-      wl-clipboard
-      ctags
-      rustc cargo go nodejs
-      sqlite ripgrep fd exa languagetool
-      zstd
-      bear
-      libqalculate
-      shellcheck
-      qt6.qtwayland
-      libsForQt5.qt5.qtwayland
-      (python311.withPackages(ps: with ps; [
-        editorconfig
-        jupyter
-        pandas
-        requests
-        pyqt6 sip qtpy qt6.qtwebengine epc lxml pyqt6-webengine # for eaf
-        qrcode # eaf-file-browser
-        pysocks # eaf-browser
-        pymupdf # eaf-pdf-viewer
-        pypinyin # eaf-file-manager
-        psutil # eaf-system-monitor
-        retry # eaf-markdown-previewer
-        markdown
-        pyte playsound #eaf-pyqterminal
-      ]))
-      texlive.combined.scheme-full
-      pandoc
-      librsvg
-      qq
-      kitty
-      wofi
-      waybar
-      clash
-      ranger
-      aspell
-      glslang
-      nixfmt
-      scrot
-      pavucontrol
-      emacs-gtk
-      networkmanagerapplet
-      neovim
-      ];
-  programs.zsh = {
-    shellAliases = {
-      update = "sudo nixos-rebuild switch";
-    };
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-        # { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-      ];
-    };
-    # autosuggestions.enable = true;
-  };
-  services.emacs = {
-      enable = true;
-      defaultEditor = true;
-  };
   };
 
   programs.zsh.enable = true;
@@ -92,6 +23,16 @@
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
+  };
+
+  #virtualisation = {
+  #  waydroid.enable = true;
+  #  lxd.enable = true;
+  #};
+
+  services.emacs = {
+      enable = true;
+      defaultEditor = true;
   };
 
 }
